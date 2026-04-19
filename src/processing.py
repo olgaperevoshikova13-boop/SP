3dc1b0d7f07629a614a9ecebb4bbe37ad1550e63
Olga <frankur1@mail.ru>
2026-04-19 22:33:44 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from mypyc.primitives.list_ops import new_list_op
+
+
 def filter_by_state(list_of_dicts,  state='EXECUTED'):
     new_list_of_dicts = []
     for item in list_of_dicts:
@@ -8,9 +11,24 @@ def filter_by_state(list_of_dicts,  state='EXECUTED'):
 
 
 if __name__ == "__main__":
-    result = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+    result = filter_by_state([
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
     print(result)
 
 
-def sort_by_date():
-    pass
+def sort_by_date(list_of_dicts, descending=True):
+    """Сортирует список словарей по дате"""
+    return sorted(list_of_dicts, key=lambda item: item['date'], reverse=descending)
+
+
+if __name__ == "__main__":
+    result = sort_by_date([
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ])
+    print(result)
